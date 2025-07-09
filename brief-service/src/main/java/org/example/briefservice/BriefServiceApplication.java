213f940c88c7136066377d810e4a9df96ad9fe4b
SamiChafik <sami.chafik00@gmail.com>
2025-07-09 09:45:27 +0100

    project struct

---
+++ brief-service/src/main/java/org/example/briefservice/BriefServiceApplication.java
@@ -0,0 +1,13 @@
+package org.example.briefservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BriefServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BriefServiceApplication.class, args);
+    }
+
+}
---
+++ brief-service/src/test/java/org/example/briefservice/BriefServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.briefservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BriefServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
