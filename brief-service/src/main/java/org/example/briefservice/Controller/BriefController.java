13271a4f4395547ad2c194ef336d0ea7909c3883
SamiChafik <sami.chafik00@gmail.com>
2025-07-10 11:33:35 +0100

    brief crud from branch farid

---
+++ brief-service/src/main/java/org/example/briefservice/Controller/BriefController.java
@@ -0,0 +1,42 @@
+package org.example.briefservice.Controller;
+
+
+import org.example.briefservice.Dto.BriefDto;
+import org.example.briefservice.Service.BriefService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/brief")
+public class BriefController {
+
+    @Autowired
+    private BriefService service;
+
+    @PostMapping
+    public BriefDto saveBrief(@RequestBody BriefDto dto){
+        return service.saveBrief(dto);
+    }
+
+    @GetMapping("/{id}")
+    public BriefDto getBriefById(@PathVariable Long id){
+        return service.getBriefById(id);
+    }
+
+    @GetMapping
+    public List<BriefDto> getAllBriefs(){
+        return service.getAllBriefs();
+    }
+
+    @PutMapping("/{id}")
+    public BriefDto editBrief(@PathVariable Long id,@RequestBody BriefDto dto){
+        return service.editBrief(id, dto);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteBrief(@PathVariable Long id){
+        service.deleteBrief(id);
+    }
+}
