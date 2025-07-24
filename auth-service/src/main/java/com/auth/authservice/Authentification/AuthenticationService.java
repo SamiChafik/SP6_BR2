672de64ca9f060993a36c764ac7abf3228fe3e05
SamiChafik <sami.chafik00@gmail.com>
2025-07-24 09:51:12 +0100

    added auth service (still testing)

---
+++ auth-service/src/main/java/com/auth/authservice/Authentification/AuthenticationService.java
@@ -0,0 +1,71 @@
+package com.auth.authservice.Authentification;
+
+
+import com.auth.authservice.Configuration.JwtService;
+import com.auth.authservice.Model.Admin;
+import com.auth.authservice.Model.Aprenant;
+import com.auth.authservice.Model.Formateur;
+import com.auth.authservice.Model.User;
+import com.auth.authservice.Repository.UserRepository;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthenticationService {
+    private final UserRepository repository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+
+    public AuthenticationService(UserRepository userRepository, PasswordEncoder passwordEncoder, JwtService jwtService, AuthenticationManager authenticationManager) {
+        this.repository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.jwtService = jwtService;
+        this.authenticationManager = authenticationManager;
+    }
+
+
+    public AuthenticationResponse register(RegisterRequest request) {
+        User user;
+
+        switch (request.getRole()) {
+            case ADMIN -> user = new Admin();
+            case APPRENANT -> user = new Aprenant();
+            case FORMATEUR -> user = new Formateur();
+
+            default -> throw new IllegalArgumentException("Invalid role: " + request.getRole());
+        }
+
+        user.setName(request.getName());
+        user.setEmail(request.getEmail());
+        user.setPassword(passwordEncoder.encode(request.getPassword()));
+        user.setRole(request.getRole());
+
+        repository.save(user);
+
+        String jwtToken = jwtService.generateToken(user);
+
+        AuthenticationResponse response = new AuthenticationResponse();
+        response.setToken(jwtToken);
+        return response;
+    }
+
+
+    public AuthenticationResponse authenticate(AuthenticationRequest request) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail(),
+                        request.getPassword()
+                )
+        );
+
+        User user = repository.findByEmail(request.getEmail());
+        String jwtToken = jwtService.generateToken(user);
+
+        AuthenticationResponse response = new AuthenticationResponse();
+        response.setToken(jwtToken);
+        return response;
+    }
+}
---
+++ auth-service/src/main/java/com/auth/authservice/Configuration/SecurityConfiguration.java
@@ -0,0 +1,46 @@
+package com.auth.authservice.Configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfiguration {
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    private final AuthenticationProvider authenticationProvider;
+
+    public SecurityConfiguration(JwtAuthenticationFilter jwtAuthenticationFilter, AuthenticationProvider authenticationProvider) {
+        this.jwtAuthenticationFilter = jwtAuthenticationFilter;
+        this.authenticationProvider = authenticationProvider;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests( auth -> auth
+                                .requestMatchers("/auth/**").permitAll()
+                                .requestMatchers("/rendu/**").hasAnyRole("APRENANT", "ADMIN")
+                        .requestMatchers("/competence/**").hasAnyRole("APRENANT", "ADMIN")
+                        .requestMatchers("/**").permitAll()
+                                .anyRequest()
+                                .authenticated()
+                )
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .cors(Customizer.withDefaults())
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
+}
